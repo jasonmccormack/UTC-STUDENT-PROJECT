5fcda888775b53a8457e04ddf57dbb2618bde99b
rootfinlay <43293464+rootfinlay@users.noreply.github.com>
2019-07-11 10:45:33 +0100

    Update models.py

--- TeamX/TeamXapp/models.py
+++ TeamX/TeamXapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class allUsers(models.Model):
+    firstName = CharField(max_length=30, verbose_name="First name: ")
+    secondName = CharField(max_length=30, verbose_name="Second name: ")
+    scrumTeam = ForeignKey("scrumTeam", on_delete=models.CASCADE, verbose_name="Scrum team: ", null=True, blank=True)
+    scrumTeamRole = ForeignKey("scrumTeam", on_delete=models.CASCADE, verbose_name="Scrum team role: ", null=True, blank=True)
+
+    def __str__(self):
+        return self.firstName
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+class scrumTeam(models.Model):
+    teamName = CharField(max_length=30, verbose_name="scrum team name: ")
