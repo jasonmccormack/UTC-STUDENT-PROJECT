128541018e6a39f22db30308430a52d68c9ce803
Abzzdev <42191357+AbzDev@users.noreply.github.com>
2019-07-11 10:13:26 +0100

    Basic front end templates and changes to settings to allow for templates and remote connecting to localhost

--- TeamX/TeamX/settings.py
+++ TeamX/TeamX/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yy46x*alg@w$vimxok@em^6me$qe!+4#c3g%l6x$w2#%2ec$^t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'TeamX.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
