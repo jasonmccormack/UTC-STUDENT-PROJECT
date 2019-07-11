2decba38f17dfc46b650120e909593fb53d8793b
Abzzdev <42191357+AbzDev@users.noreply.github.com>
2019-07-11 10:20:10 +0100

    Edited URL's and views to work with front end

--- TeamX/TeamX/urls.py
+++ TeamX/TeamX/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from TeamXapp.views import (
+    base,
+    index,
+    landingpage
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('', base),
 ]
