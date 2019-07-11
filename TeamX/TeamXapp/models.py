c183238a14553b6d53dab4e04c53d1396437af24
rootfinlay <43293464+rootfinlay@users.noreply.github.com>
2019-07-11 16:13:20 +0100

    Fixing stuff

--- TeamX/TeamXapp/models.py
+++ TeamX/TeamXapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class AllMembers(models.Model):
     firstName = models.CharField(max_length=30, verbose_name="First name: ")
-    secondName = models.CharField(max_length=30, verbose_name="Second name: ") 
+    secondName = models.CharField(max_length=30, verbose_name="Second name: ")
     scrumTeam = models.ForeignKey("ScrumTeam", on_delete=models.CASCADE, verbose_name="Scrum team: ", null=True, blank=True)
     scrum_team_roles = models.ForeignKey("ScrumTeamRole", on_delete=models.CASCADE, verbose_name="Scrum Team Roles: ", null=True, blank=True)
     myskill = models.ManyToManyField('skills', blank=True, verbose_name="Skills")
@@ -23,12 +23,12 @@ class AllMembers(models.Model):
 class ScrumTeam(models.Model):
     teamName = models.CharField(max_length=30, verbose_name="scrum team name: ")
     description = models.TextField(blank=True, null=True)
-    scrum_master = models.ForeignKey("AllMembers", on_delete=models.CASCADE, null=True, blank=True)
+    scrum_master = models.ForeignKey("AllMembers", on_delete=models.CASCADE, verbose_name="Scrum master: ", null=True, blank=True)
     #domain = models.ForeignKey('Domain', null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__ (self):
         return self.teamName
-    
+
     class Meta:
         verbose_name = 'Scrum Team'
         verbose_name_plural = 'Scrum Teams'
@@ -47,7 +47,7 @@ class AdminAccounts(models.Model):
 
 
 class Skills(models.Model):
-    
+
     skill = models.CharField(max_length=30, blank=True)
 
     def __str__(self):
