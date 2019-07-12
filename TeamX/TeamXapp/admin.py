985467cd0e7631a451395f076dd935824ad8240a
Abzzdev <42191357+AbzDev@users.noreply.github.com>
2019-07-12 09:52:20 +0100

    Added filter support and patched start and leave time

--- TeamX/TeamXapp/admin.py
+++ TeamX/TeamXapp/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import AllMembers, AdminAccounts, Skills, ScrumTeam, ScrumTeamRole, WorkPattern, ScrumTeamStatus, ScrumTeamType, Domain
+from .models import AllMembers, AdminAccounts, Skills, ScrumTeam, ScrumTeamRole, WorkPattern, LeaveStatus, LeaveStartTime, \
+    LeaveEndTime, ScrumTeamStatus, ScrumTeamType, Domain
 
 # Admin
 class AllMemberAdmin(admin.ModelAdmin):
     list_display = (['first_name', 'second_name', 'scrum_team_name', 'leave_status', 'leave_start_time', 'leave_end_time'])
     search_fields = (['scrum_team_name', 'first_name', 'second_name'])
-    list_filter = (['scrum_team_name'])
+    list_filter = (['scrum_team_name', 'leave_status'])
 
 class TeamAdmin(admin.ModelAdmin):
     list_display = (['name', 'scrum_master', 'status', 'current_focus',])
@@ -23,3 +24,6 @@ admin.site.register(WorkPattern)
 admin.site.register(ScrumTeamType)
 admin.site.register(ScrumTeamStatus)
 admin.site.register(Domain)
+admin.site.register(LeaveStatus)
+admin.site.register(LeaveStartTime)
+admin.site.register(LeaveEndTime)
