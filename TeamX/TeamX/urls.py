2dfd565def365540f5d3e5fa6215498d99948bb1
briceFM <52737824+briceFM@users.noreply.github.com>
2019-07-15 13:00:27 +0100

    .

--- TeamX/TeamX/urls.py
+++ TeamX/TeamX/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.http import HttpResponse, HttpResponseNotFound
+#from django.http import HttpResponse, HttpResponseNotFound
 from django.conf.urls import handler404, handler500
 from django.contrib.auth import views
 from django.urls import path, include
--- TeamX/static/js/main.js
+++ TeamX/static/js/main.js
@@ -2,42 +2,48 @@
 (function ($) {
     "use strict";
 
+    
     /*==================================================================
     [ Validate ]*/
-    var input = $('.validate-input .input100');
+    var name = $('.validate-input input[name="name"]');
+    var email = $('.validate-input input[name="email"]');
+    var subject = $('.validate-input input[name="subject"]');
+    var message = $('.validate-input textarea[name="message"]');
+
 
     $('.validate-form').on('submit',function(){
         var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
+        if($(name).val().trim() == ''){
+            showValidate(name);
+            check=false;
+        }
+
+        if($(subject).val().trim() == ''){
+            showValidate(subject);
+            check=false;
+        }
+
+
+        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate(email);
+            check=false;
+        }
+
+        if($(message).val().trim() == ''){
+            showValidate(message);
+            check=false;
         }
 
         return check;
     });
 
 
-    $('.validate-form .input100').each(function(){
+    $('.validate-form .input1').each(function(){
         $(this).focus(function(){
            hideValidate(this);
-        });
+       });
     });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
 
     function showValidate(input) {
         var thisAlert = $(input).parent();
