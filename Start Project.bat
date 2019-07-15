918cdfacc9aa49f4db34ed882d49557cb7a47b96
briceFM <52737824+briceFM@users.noreply.github.com>
2019-07-15 11:28:01 +0100

    .

--- Start Project.bat
+++ Start Project.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /
 REM This points to the correct directory for a local github install of the project.
-cd C:\Users\USERNAMEHERE\Documents\GitHub\UTC-STUDENT-PROJECT\TeamX
+cd C:\Users\Student3\Documents\GitHub\UTC-STUDENT-PROJECT\TeamX
 python manage.py runserver 0.0.0.0:8000
 pause
