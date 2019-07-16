707f9800405cf9ea941e2b1cda66acac0b7f2ecf
Abzzdev <42191357+AbzDev@users.noreply.github.com>
2019-07-16 11:35:30 +0100

    Update models.py

--- TeamX/TeamXapp/models.py
+++ TeamX/TeamXapp/models.py
@@ -121,6 +121,7 @@ class ScrumTeamStatus(models.Model):
 
 class Domain(models.Model):
     domain_name = models.CharField(max_length=30, null=True, blank=True)
+    description = models.TextField(blank=True, null=True, verbose_name="Description")
 
     def __str__(self):
         return self.domain_name
