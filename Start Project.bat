44256c66bee527861edc71a038934d13f880646c
briceFM <52737824+briceFM@users.noreply.github.com>
2019-07-17 08:40:06 +0100

    .

--- Start Project.bat
+++ Start Project.bat
@@ -2,7 +2,7 @@
 cd /
 REM This points to the correct directory for a local github install of the project.
 cd C:\Users\Student3\Documents\GitHub\UTC-STUDENT-PROJECT\TeamX
-python manage.py makemigrations
 python manage.py migrate
+python manage.py makemigrations
 python manage.py runserver
 pause
