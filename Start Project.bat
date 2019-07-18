4d8c1a2b5850f022a396871e118e37df81ae7f5f
Abzzdev <42191357+AbzDev@users.noreply.github.com>
2019-07-18 13:35:38 +0100

    Update Start Project.bat

--- Start Project.bat
+++ Start Project.bat
@@ -1,10 +1,8 @@
 @echo off
 cd /
 REM This points to the correct directory for a local github install of the project.
-cd C:\Users\Student3\Documents\GitHub\UTC-STUDENT-PROJECT\UTC-STUDENT-PROJECT-master\TeamX
+cd C:\Users\USERNAME HERE\Documents\GitHub\UTC-STUDENT-PROJECT\TeamX
+python manage.py makemigrations
 python manage.py migrate
-python manage.py makemigrations --merge
-python manage.py migrate
-python manage.py makemigrations 
 python manage.py runserver
 pause
