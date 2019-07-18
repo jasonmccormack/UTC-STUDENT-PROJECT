1661c2a1297e0cefbd5459a7aec2cbae9c34b70a
jasonmccormack <jason.mccormack@sage.com>
2019-07-18 03:55:32 +0100

    add people views

--- TeamX/TeamXapp/admin.py
+++ TeamX/TeamXapp/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import AllMembers, Skills, ScrumTeam, ScrumTeamRole, LeaveStatus , ScrumTeamStatus, ScrumTeamType, Domain, LeaveCalendar, JobRoleGroup
+from .models import AllMembers, Skills, ScrumTeam, ScrumTeamRole, LeaveStatus , ScrumTeamStatus,ScrumTeamType, Domain, LeaveCalendar, JobRoleGroup, location
 
 # Admin
 class AllMemberAdmin(admin.ModelAdmin):
-    list_display = (['first_name', 'second_name', 'scrum_team_name', 'work_pattern'])
+    list_display = (['first_name', 'second_name', 'scrum_team_name','scrum_team_roles','location', 'work_pattern'])
     search_fields = (['scrum_team_name', 'first_name', 'second_name'])
-    list_filter = (['scrum_team_name', 'in_team', 'scrum_team_roles', 'work_pattern'])
+    list_filter = (['scrum_team_name', 'scrum_team_roles', 'work_pattern', 'location'])
 
 class TeamAdmin(admin.ModelAdmin):
     list_display = (['name', 'scrum_master', 'status', 'current_focus',])
@@ -37,6 +37,8 @@ class LeaveCalendarAdmin(admin.ModelAdmin):
     list_display = (["team_member", "start_date", "end_date", "leave_type", "note"])
     list_filter = (["team_member"])
 
+class location_admin(admin.ModelAdmin):
+    list_display = (["location_name", "country"])
 
 # Register your models here.
 admin.site.register(AllMembers, AllMemberAdmin)
@@ -49,3 +51,4 @@ admin.site.register(Domain, DomainAdmin)
 admin.site.register(LeaveStatus, LeaveStatusAdmin)
 admin.site.register(LeaveCalendar, LeaveCalendarAdmin)
 admin.site.register(JobRoleGroup)
+admin.site.register(location, location_admin)
